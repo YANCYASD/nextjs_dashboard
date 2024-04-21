344a0e270925e7e6b9b20e396b39fe1c5a91c447
YXin <1036322011@qq.com>
2024-04-21 20:03:54 +0800

    init p

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+      <main>
+        <h1>
+          this is invoices page
+        </h1>
+      </main>
+    );
+  }
+  
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>this is dashboard page</h1>
+    </main>
+  );
+}
