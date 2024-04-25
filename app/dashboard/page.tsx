f0cd21dd3b1a8c05c0c7098611d51e85c632b5b5
YXin <1036322011@qq.com>
2024-04-25 14:19:36 +0800

    fix: build fail

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -35,9 +35,7 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* @ts-expect-error Server Component */}
         <RevenueChart revenue={revenue} />
-        {/* @ts-expect-error Server Component */}
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
