8c65f3368bb998f03329ce9698773dff501e64ff
YXin <1036322011@qq.com>
2024-04-30 17:23:51 +0800

    feat: create&update invoices

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <main>create page</main>;
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+import { Suspense } from 'react';
+
+export default async function Page() {
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Suspense fallback={'loading'}>
+        {/* @ts-ignore */}
+        <Form />
+      </Suspense>
+    </main>
+  );
 }
